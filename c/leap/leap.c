1c5a226a5ce14774a7746363e4e24652f9eee96d
Antoine Marie <antoinekodjoh@yahoo.com>
2024-11-09 17:50:15 +0100

    UPDATE

--- c/leap/leap.c
+++ c/leap/leap.c
@@ -1,1 +1,26 @@
 #include "leap.h"
+
+bool leap_year(int year)
+{
+
+    int cond1 = year % 4;
+    int cond2 = year % 100;
+    int cond3 = year % 400;
+
+    if (cond1 == 0)
+    {
+        if ((cond2 == 0 && cond3 == 0) || (cond2 != 0))
+        {
+          return true;
+        }
+        else
+        {
+        return false;
+        }
+    }    
+    else
+    {
+        return false;
+    }
+    
+}
